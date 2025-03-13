630815c9ff7fb2a019e6ae216d5be53b480042aa
Nicky Obreykov <nicky@areachica.se>
2025-03-13 21:53:09 +0100

    Add no-neck-pain.nvim

--- init.lua
+++ init.lua
@@ -1049,6 +1049,20 @@ require('lazy').setup({
     end,
   },
 
+  {
+    'shortcuts/no-neck-pain.nvim',
+    opts = {
+      width = 150,
+
+      autocmds = {
+        enableOnVimEnter = true,
+        enableOnTabEnter = true,
+        enableOnColorSchemeChange = true,
+        skipEnteringNoNeckPainBuffer = true,
+      },
+    },
+  },
+
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
